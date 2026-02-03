575cca3af3def5ccd5383a68934db614bfaea850
jeffy-d-martin <jeffydmartin@gmail.com>
2026-02-03 21:43:36 +0530

    3.02.2026

--- Sort/CyclicSort.java
+++ Sort/CyclicSort.java
@@ -2,21 +2,22 @@ package Sort;
 import java.util.*;
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] array = { 5 , 3 , 4 , 2 , 0 , 1};
+        int[] array = { 5 , 3 , 4 , 2 , 1};
         System.out.println("The Sorted Arrays is : " + Arrays.toString(cycleSort( array )));
     }
 
     private static int[]  cycleSort(int[] array) {
-        int i = 0 ;
-        while( i < array.length){
-            int correct = array[i];
-            if(correct != i){
-               int temp = array[correct];
-               array[correct] = array[i];
-               array[i] = temp;
+        int index = 0;
+        while ( index < array.length ){
+            int correct = array[index] - 1;
+            if( array[correct] != array[index] ){
+                int temp = array[index];
+                array[index] = array[correct];
+                array[correct] = temp;
             }else{
-                i++;
+                index++;
             }
+
         }
         return array;
     }
